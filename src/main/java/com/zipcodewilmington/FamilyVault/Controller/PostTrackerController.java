c6930bb240c6be66810111738d90f79065e930ab
DeeptiMishra@1512 <mishra.deeptimishra@gmail.com>
2024-12-18 13:29:11 -0500

    Commit changes.

--- src/main/java/com/zipcodewilmington/FamilyVault/Controller/PostTrackerController.java
+++ src/main/java/com/zipcodewilmington/FamilyVault/Controller/PostTrackerController.java
@@ -1,4 +1,28 @@
 package com.zipcodewilmington.FamilyVault.Controller;
 
+import com.zipcodewilmington.FamilyVault.Entity.Media;
+import com.zipcodewilmington.FamilyVault.Entity.PostTracker;
+import com.zipcodewilmington.FamilyVault.Service.PostTrackerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class PostTrackerController {
+    @Autowired
+    private PostTrackerService postTrackerService;
+
+    @GetMapping("/getPost")
+    public Media getMedia(@RequestParam String postId){
+        return postTrackerService.getPost(postId);
+    }
+
+
+    @PostMapping("/savePost")
+    public PostTracker savePost(@RequestBody PostTracker postTracker){
+        return postTrackerService.savePost(postTracker);
+    }
+
+
+
+
 }
--- src/main/java/com/zipcodewilmington/FamilyVault/Service/PostTrackerService.java
+++ src/main/java/com/zipcodewilmington/FamilyVault/Service/PostTrackerService.java
@@ -1,4 +1,11 @@
 package com.zipcodewilmington.FamilyVault.Service;
 
+import com.zipcodewilmington.FamilyVault.Entity.PostTracker;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface PostTrackerService {
+
+    PostTracker getPost(PostTracker postId);
+    PostTracker savePost(PostTracker postTracker);
 }
