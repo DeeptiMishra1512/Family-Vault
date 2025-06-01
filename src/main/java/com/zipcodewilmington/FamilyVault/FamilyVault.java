0aa2aae7a41628c877bf90c0b853b388ebc2f13b
DeeptiMishra1512 <mishra.deeptimishra@gmail.com>
2025-06-01 01:26:21 -0400

    Commit changes for #Caching Feature -  Media files being cached in-memory.
    Signed-off-by: DeeptiMishra1512 <mishra.deeptimishra@gmail.com>

--- src/main/java/com/zipcodewilmington/FamilyVault/FamilyVault.java
+++ src/main/java/com/zipcodewilmington/FamilyVault/FamilyVault.java
@@ -2,8 +2,10 @@ package com.zipcodewilmington.FamilyVault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FamilyVault {
 
     public static void main(String[] args) {
--- src/main/java/com/zipcodewilmington/FamilyVault/Service/impl/S3MediaService.java
+++ src/main/java/com/zipcodewilmington/FamilyVault/Service/impl/S3MediaService.java
@@ -1,6 +1,9 @@
 package com.zipcodewilmington.FamilyVault.Service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.core.ResponseBytes;
@@ -22,9 +25,10 @@ import java.util.Optional;
 @Service
 public class S3MediaService {
 
+    private static final Logger log = LoggerFactory.getLogger(S3MediaService.class);
     private final S3Client s3Client;
     private final String bucketName;
-    private final S3Presigner s3Presigner;
+ //   private final S3Presigner s3Presigner;
 
     public S3MediaService(@Value("${aws.region}") String region,
                           @Value("${aws.s3.bucketName}") String bucketName) {
@@ -33,10 +37,10 @@ public class S3MediaService {
                 .region(Region.of(region))
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
-        this.s3Presigner = S3Presigner.builder()
-                .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
+//        this.s3Presigner = S3Presigner.builder()
+//                .region(Region.of(region))
+//                .credentialsProvider(DefaultCredentialsProvider.create())
+//                .build();
     }
 
 //    public URL generatePresignedUrl(String key, int expirationMinutes) {
@@ -74,8 +78,10 @@ public class S3MediaService {
     }
 
     /** Service to fetch media file content from S3 bucket */
+    @Cacheable(value = "s3MediaCache", key = "#key")
     public Optional<ResponseBytes<GetObjectResponse>> getMediaFile(String key) {
         try {
+            log.info("Inside S3 Service class");
             GetObjectRequest getRequest = GetObjectRequest.builder()
                     .bucket(bucketName)
                     .key(key)
